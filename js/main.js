9728974933445e563e61edaa1d4a1d8033cd5b71
Admin1 <altbio22@yandex.ru>
2023-06-29 15:58:59 +0300

    s

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const swiper = new Swiper(".swiper", {
-    effect: "fade",
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-itme');
+
+tabItem.forEach(function(el){
+  el.addEventListener('click', open)
+});
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+
+  tabItem.forEach(function(item) {
+    item.classList.remove('tabs__btn-item--active')
   });
+
+  tabTarget.classList.add('tabs__btn-item--active');
+
+};
+
+
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+});
